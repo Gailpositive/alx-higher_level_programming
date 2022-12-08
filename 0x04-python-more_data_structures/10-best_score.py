65c58e00e6ccf2a3d6b3cff041958f5cd5fb7fde
Gailpositive <stormbrain2000@gmail.com>
2022-12-08 14:08:36 -0800

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+
+
+# best_score - retuns a key with the biggest integer value
 def best_score(a_dictionary):
-    if a_dictionary is None or not a_dictionary:
-        return 'None'
+    if a_dictionary:
+        dlist = list(a_dictionary)
+        largK = dlist[0]
+        for i in dlist:
+            if a_dictionary[largK] < a_dictionary[i]:
+                largK = i
+        return largK
     else:
-        return max(a_dictionary, key=lambda x: a_dictionary[x])
+        return
 
